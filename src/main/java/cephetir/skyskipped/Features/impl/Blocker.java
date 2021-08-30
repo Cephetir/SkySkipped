594dbc673836daf12b83e0d48f660e024502ae3b
Cephetir <tupoluboy@gmail.com>
2021-08-30 20:53:03 +0300

    Added GS blocker

---
+++ src/main/java/cephetir/skyskipped/Features/impl/Blocker.java
@@ -0,0 +1,41 @@
+/*
+ *
+ * DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ * Version 2, December 2004
+ *
+ * Copyright (C) 2021 Cephetir
+ *
+ * Everyone is permitted to copy and distribute verbatim or modified
+ * copies of this license document, and changing it is allowed as long
+ * as the name is changed.
+ *
+ * DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ * TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+ *
+ *  0. You just DO WHAT THE FUCK YOU WANT TO.
+ */
+
+package cephetir.skyskipped.Features.impl;
+
+import cephetir.skyskipped.Features.Feature;
+import cephetir.skyskipped.config.Cache;
+import cephetir.skyskipped.config.Config;
+import cephetir.skyskipped.utils.TextUtils;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+public class Blocker extends Feature {
+    public Blocker() {
+        super("Blocker", "Slayers", "Blocks ability for specific items");
+    }
+
+    @SubscribeEvent
+    public void block(PlayerInteractEvent event) {
+        if (!Cache.inSkyblock || !Config.gsBlock) return;
+        if (event.entity != mc.thePlayer || event.action == PlayerInteractEvent.Action.LEFT_CLICK_BLOCK) return;
+        String item = TextUtils.stripColor(mc.thePlayer.getHeldItem().getDisplayName());
+        if (item == null || !item.contains("Giant's Sword")) return;
+        mc.thePlayer.playSound("random.orb", 0.8f, 1f);
+        event.setCanceled(true);
+    }
+}
--- src/main/java/cephetir/skyskipped/config/Config.java
+++ src/main/java/cephetir/skyskipped/config/Config.java
@@ -1,19 +1,18 @@
 /*
- * SkySkipped - Hypixel Skyblock mod
- * Copyright (C) 2021  Cephetir
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ * Version 2, December 2004
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
+ * Copyright (C) 2021 Cephetir
  *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * Everyone is permitted to copy and distribute verbatim or modified
+ * copies of this license document, and changing it is allowed as long
+ * as the name is changed.
+ *
+ * DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ * TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+ *
+ *  0. You just DO WHAT THE FUCK YOU WANT TO.
  */
 
 package cephetir.skyskipped.config;
@@ -64,6 +63,14 @@ public class Config extends Vigilant {
             description = "Shows players through walls."
     )
     public static boolean playerESP = false;
+
+    @Property(
+            type = PropertyType.SWITCH,
+            name = "Block GS ability",
+            category = "Slayers", subcategory = "Slayers",
+            description = "Blocks Giant's sword ability."
+    )
+    public static boolean gsBlock = false;
 
     public Config() {
         super(new File("./config/skyskipped.toml"));
