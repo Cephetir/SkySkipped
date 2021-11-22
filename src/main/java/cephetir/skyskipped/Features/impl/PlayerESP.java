cfacec742a06e0c61cf16a9c2a00ef35ae90b497
Cephetir <silyichev@gmail.com>
2021-11-22 16:53:42 +0300

    Mixins and PlayerESP rewrite

--- src/main/java/cephetir/skyskipped/Features/impl/PlayerESP.java
+++ src/main/java/cephetir/skyskipped/Features/impl/PlayerESP.java
@@ -20,79 +20,43 @@ package cephetir.skyskipped.Features.impl;
 import cephetir.skyskipped.Features.Feature;
 import cephetir.skyskipped.config.Cache;
 import cephetir.skyskipped.config.Config;
-import net.minecraft.client.entity.AbstractClientPlayer;
-import net.minecraft.client.entity.EntityOtherPlayerMP;
-import net.minecraft.client.renderer.GlStateManager;
+import cephetir.skyskipped.listeners.events.RenderEntityModelEvent;
+import cephetir.skyskipped.utils.OutlineUtils;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.InventoryPlayer;
-import net.minecraft.item.ItemStack;
 import net.minecraft.scoreboard.ScorePlayerTeam;
 import net.minecraft.scoreboard.Team;
-import net.minecraftforge.client.event.RenderPlayerEvent;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import org.lwjgl.opengl.GL11;
+
+import java.util.ArrayList;
 
 public class PlayerESP extends Feature {
-    private boolean preCalled = false;
+    private final ArrayList<Entity> highlightedEntities = new ArrayList<>();
 
     public PlayerESP() {
         super("Player ESP", "Dungeons", "Shows players through walls.");
     }
 
-    @SuppressWarnings("all")
     @SubscribeEvent
-    public void onEntityRenderPre(RenderPlayerEvent.Pre event) {
-        if (preCalled || !Config.playerESP || !Cache.isInDungeon) return;
-        if (event.entityPlayer instanceof EntityOtherPlayerMP) return;
-        ScorePlayerTeam scoreplayerteam = (ScorePlayerTeam) event.entityPlayer.getTeam();
-        if (scoreplayerteam != null && scoreplayerteam.getNameTagVisibility() != Team.EnumVisible.NEVER) ;
-        else return;
-        preCalled = true;
+    public void onEntityJoinWorld(final EntityJoinWorldEvent event) {
+        if (!Cache.isInDungeon || !(event.entity instanceof EntityPlayer)) return;
+        ScorePlayerTeam scoreplayerteam = (ScorePlayerTeam) ((EntityPlayer) event.entity).getTeam();
+        if (scoreplayerteam == null || scoreplayerteam.getNameTagVisibility() == Team.EnumVisible.NEVER) return;
+        event.entity.setInvisible(false);
+        highlightedEntities.add(event.entity);
+    }
 
-        GL11.glEnable(GL11.GL_STENCIL_TEST);
-        GL11.glClearStencil(0);
-        GL11.glClear(GL11.GL_STENCIL_BUFFER_BIT);
+    @SubscribeEvent
+    public void onRenderEntityModel(RenderEntityModelEvent event) {
+        if (!Config.playerESP | !Cache.isInDungeon || highlightedEntities.isEmpty() || !highlightedEntities.contains(event.entity))
+            return;
+        OutlineUtils.outlineEntity(event);
+    }
 
-        GL11.glStencilMask(0xFF);
-        GL11.glStencilFunc(GL11.GL_ALWAYS, 1, 0xFF);
-        GL11.glStencilOp(GL11.GL_KEEP, GL11.GL_REPLACE, GL11.GL_REPLACE);
-
-        EntityPlayer entity = event.entityPlayer;
-        InventoryPlayer inv = entity.inventory;
-        ItemStack[] armor = inv.armorInventory;
-        inv.armorInventory = new ItemStack[4];
-        ItemStack[] hand = inv.mainInventory;
-        inv.mainInventory = new ItemStack[36];
-
-        float f = entity.prevRotationYaw + (entity.rotationYaw - entity.prevRotationYaw) * event.partialRenderTick;
-        try {
-            event.renderer.doRender((AbstractClientPlayer) event.entityPlayer, event.x, event.y, event.z, f, event.partialRenderTick);
-        } catch (Throwable ignored) {
-        }
-
-        GL11.glStencilOp(GL11.GL_KEEP, GL11.GL_KEEP, GL11.GL_KEEP);
-        GL11.glStencilFunc(GL11.GL_NOTEQUAL, 1, 0xff);
-        GL11.glDepthMask(false);
-        GL11.glDepthFunc(GL11.GL_GEQUAL);
-
-        GlStateManager.pushMatrix();
-        GlStateManager.translate(event.x, event.y + 0.9, event.z);
-        GlStateManager.scale(1.2f, 1.1f, 1.2f);
-        event.renderer.setRenderOutlines(true);
-        try {
-            event.renderer.doRender((AbstractClientPlayer) event.entityPlayer, 0, -0.9, 0, f, event.partialRenderTick);
-        } catch (Throwable ignored) {
-        }
-
-        event.renderer.setRenderOutlines(false);
-        GL11.glDepthFunc(GL11.GL_LEQUAL);
-        GlStateManager.popMatrix();
-
-        GL11.glDisable(GL11.GL_STENCIL_TEST); // Turn this shit off!
-
-        inv.armorInventory = armor;
-        inv.mainInventory = hand;
-
-        preCalled = false;
+    @SubscribeEvent
+    public void onWorldLoad(WorldEvent.Load event) {
+        highlightedEntities.clear();
     }
 }
--- src/main/java/cephetir/skyskipped/config/Config.java
+++ src/main/java/cephetir/skyskipped/config/Config.java
@@ -22,6 +22,7 @@ import gg.essential.vigilance.Vigilant;
 import gg.essential.vigilance.data.Property;
 import gg.essential.vigilance.data.PropertyType;
 
+import java.awt.*;
 import java.io.File;
 import java.util.function.Consumer;
 
@@ -68,6 +69,14 @@ public class Config extends Vigilant {
     public static boolean playerESP = false;
 
     @Property(
+            type = PropertyType.COLOR,
+            name = "Player ESP Color",
+            category = "Dungeons", subcategory = "Dungeons",
+            description = "Outline color for Player ESP."
+    )
+    public static Color espColor = Color.GREEN;
+
+    @Property(
             type = PropertyType.SWITCH,
             name = "Block GS ability",
             category = "Slayers", subcategory = "Slayers",
@@ -107,65 +116,21 @@ public class Config extends Vigilant {
     )
     public static String BotName = "";
 
-//    @SuppressWarnings("unused")
-//    @Property(
-//            type = PropertyType.SWITCH,
-//            name = "FPS",
-//            category = "HUD", subcategory = "HUD",
-//            description = "Shows your fps."
-//    )
     public static boolean fpsHud = false;
 
-//    @Property(
-//            type = PropertyType.NUMBER,
-//            name = "FPS Pos X",
-//            category = "HUD", subcategory = "HUD",
-//            description = "Where on screen hud will be."
-//    )
     public static int fpsPosX = 1;
 
-//    @Property(
-//            type = PropertyType.NUMBER,
-//            name = "FPS Pos Y",
-//            category = "HUD", subcategory = "HUD",
-//            description = "Where on screen hud will be."
-//    )
     public static int fpsPosY = 1;
 
-//    @SuppressWarnings("unused")
-//    @Property(
-//            type = PropertyType.SWITCH,
-//            name = "Ping",
-//            category = "HUD", subcategory = "HUD",
-//            description = "Shows your ping."
-//    )
     public static boolean pingHud = false;
 
-//    @Property(
-//            type = PropertyType.NUMBER,
-//            name = "Ping Pos X",
-//            category = "HUD", subcategory = "HUD",
-//            description = "Where on screen hud will be."
-//    )
     public static int pingPosX = 1;
 
-//    @Property(
-//            type = PropertyType.NUMBER,
-//            name = "Ping Pos Y",
-//            category = "HUD", subcategory = "HUD",
-//            description = "Where on screen hud will be."
-//    )
     public static int pingPosY = 1;
 
     private long timer = 0;
-//    public static final FPSHud fpsHudClass = new FPSHud();
-//    public static final PingHud pingHudClass = new PingHud();
     public Config() {
-        super(new File("./config/skyskipped.toml"));
-//        addDependency("fpsPosX", "fpsHud");
-//        addDependency("fpsPosY", "fpsHud");
-//        addDependency("pingPosX", "pingHud");
-//        addDependency("pingPosY", "pingHud");
+        super(new File("./config/skyskipped.toml"), "SkySkipped");
         registerListener("DRPC", (Consumer<Boolean>) aBoolean -> {
             if (aBoolean && (!RPC.getINSTANCE().getDiscordRPCManager().isActive())) {
                 if (System.currentTimeMillis() - timer < 4000) return;
@@ -175,22 +140,7 @@ public class Config extends Vigilant {
                 RPC.getINSTANCE().getDiscordRPCManager().stop();
             }
         });
-//        registerListener("fpsHud", (Consumer<Boolean>) aBoolean -> {
-//            if (aBoolean) {
-//                fpsHudClass.save(ScreenPosition.fromAbsolute(fpsPosX, fpsPosY));
-//                HUDManager.getINSTANCE().register(fpsHudClass);
-//            } else {
-//                HUDManager.getINSTANCE().unregister(fpsHudClass);
-//            }
-//        });
-//        registerListener("pingHud", (Consumer<Boolean>) aBoolean -> {
-//            if (aBoolean) {
-//                pingHudClass.save(ScreenPosition.fromAbsolute(pingPosX, pingPosY));
-//                HUDManager.getINSTANCE().register(pingHudClass);
-//            } else {
-//                HUDManager.getINSTANCE().unregister(pingHudClass);
-//            }
-//        });
+        addDependency("espColor", "playerESP");
         initialize();
     }
 }
