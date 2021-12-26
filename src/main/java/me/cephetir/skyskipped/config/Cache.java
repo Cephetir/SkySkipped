2fb06bfd2c34775af892c4762088d616273f827f
Cephetir <silyichev@gmail.com>
2021-12-26 15:05:24 +0300

    Now works

--- src/main/java/me/cephetir/skyskipped/config/Cache.java
+++ src/main/java/me/cephetir/skyskipped/config/Cache.java
@@ -22,7 +22,6 @@ public class Cache {
     public static int dungeonPercentage = 0;
     public static String dungeonName = "";
     public static int totalScore = 0;
-    public static String blood = "no";
     public static boolean was = false;
 
     public static boolean inParty = false;
