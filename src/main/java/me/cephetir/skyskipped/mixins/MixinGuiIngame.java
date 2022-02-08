2ecbc57ef0a9cf1bf8bf343f5010b7101a31ca74
Cephetir <silyichev@gmail.com>
2022-02-08 19:16:59 +0300

    Scoreboard is beautiful now!

--- src/main/java/me/cephetir/skyskipped/mixins/MixinGuiIngame.java
+++ src/main/java/me/cephetir/skyskipped/mixins/MixinGuiIngame.java
@@ -3,7 +3,7 @@
  * DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
  * Version 2, December 2004
  *
- * Copyright (C) 2021 Cephetir
+ * Copyright (C) 2022 Cephetir
  *
  * Everyone is permitted to copy and distribute verbatim or modified
  * copies of this license document, and changing it is allowed as long
@@ -20,27 +20,27 @@ package me.cephetir.skyskipped.mixins;
 import gg.essential.universal.ChatColor;
 import me.cephetir.skyskipped.config.Config;
 import me.cephetir.skyskipped.utils.TextUtils;
-import net.minecraft.scoreboard.ScorePlayerTeam;
-import net.minecraft.scoreboard.Team;
+import net.minecraft.client.gui.GuiIngame;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 import java.text.DecimalFormat;
 
-@Mixin(ScorePlayerTeam.class)
-public class MixinScorePlayerTeam {
+@Mixin(GuiIngame.class)
+public class MixinGuiIngame {
 
-    @Inject(method = "formatPlayerName", at = @At(value = "RETURN"), cancellable = true)
-    private static void formatPlayerName(Team p_96667_0_, String p_96667_1_, CallbackInfoReturnable<String> cir) {
-        String text = TextUtils.keepScoreboardCharacters(TextUtils.stripColor(cir.getReturnValue())).trim();
-        if (Config.Companion.getCoinsToggle() && text.startsWith("Purse: ")) {
-            double coins = Double.parseDouble(text.substring(7).split(" ")[0].replace(",", ""));
+    @ModifyArg(method = "renderScoreboard", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/FontRenderer;drawString(Ljava/lang/String;III)I"), index = 0)
+    public String a(String text) {
+        String txt = TextUtils.keepScoreboardCharacters(TextUtils.stripColor(text)).trim();
+        if (Config.Companion.getCustomSb() && txt.equalsIgnoreCase("www.hypixel.net"))
+            return Config.Companion.getCustomSbText().replace("&", "§");
+        else if (Config.Companion.getCoinsToggle() && txt.startsWith("Purse: ")) {
+            double coins = Double.parseDouble(txt.substring(7).split(" ")[0].replace(",", ""));
             double needed = coins + Config.Companion.getCoins();
             DecimalFormat format = new DecimalFormat("###,###.##");
             String s = format.format(needed).replace(" ", ",");
-            cir.setReturnValue("Purse: " + ChatColor.GOLD + s);
-        }
+            return "Purse: " + ChatColor.GOLD + s;
+        } else return text;
     }
 }
