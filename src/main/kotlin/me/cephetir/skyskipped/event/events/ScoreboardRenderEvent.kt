7f7f9ca9f3a4a466286b37d80dfbf8a3f7aa2050
Cephetir <silyichev@gmail.com>
2022-02-16 19:28:12 +0300

    Custom Scoreboard

---
+++ src/main/kotlin/me/cephetir/skyskipped/event/events/ScoreboardRenderEvent.kt
@@ -0,0 +1,26 @@
+/*
+ *
+ * DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ * Version 2, December 2004
+ *
+ * Copyright (C) 2022 Cephetir
+ *
+ * Everyone is permitted to copy and distribute verbatim or modified
+ * copies of this license document, and changing it is allowed as long
+ * as the name is changed.
+ *
+ * DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ * TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+ *
+ *  0. You just DO WHAT THE FUCK YOU WANT TO.
+ */
+
+package me.cephetir.skyskipped.event.events
+
+import net.minecraft.client.gui.ScaledResolution
+import net.minecraft.scoreboard.ScoreObjective
+import net.minecraftforge.fml.common.eventhandler.Cancelable
+import net.minecraftforge.fml.common.eventhandler.Event
+
+@Cancelable
+class ScoreboardRenderEvent(val objective: ScoreObjective, var resolution: ScaledResolution) : Event()
